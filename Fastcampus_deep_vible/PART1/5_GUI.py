3326c14e5eee8ad0ae6bac36d0612919dfae3653
RaspberryPanda <63005513+asdfrv20@users.noreply.github.com>
2021-07-06 19:42:01 +0900

    GUI practiec 'overwatch character manager' commit

--- Fastcampus_deep_vible/PART1/5_GUI.py
+++ Fastcampus_deep_vible/PART1/5_GUI.py
@@ -16,46 +16,54 @@ selected_index = 0     # selected_index: 선택하는 항목의 인덱스를 저
 
 # 이벤트 함수들 정의 
 def stationfares_selected(event):  
-    # stationfares_selected만 다른 이벤트 함수들과는 다르고 event를 입력으로 받아야 한다. 
-    # tree에서 하나가 선택되면 해당 데이터가 station과 fare값에 나누어 들어가고 
-    # 선택된 항목은 tree에서 지워지도록 동작해주는 함수
-    # 교육목적상 아래 부분에서 잘 실행되지 않도록 설정하셨다고 함. 
-    global selected_index   # (중요!) 이 함수안에서 설정된 selecetd_index가 전역변수임을 알려주어야한다.(만약 그렇지 않을 경우 함수 종료와 함께 값 없어짐)
+    # stationfares_selected는 이벤트에 대한 핸들 함수이므로, event를 입력으로 받아주어야 한다. 
+    # tree에서 하나가 선택되면 해당 데이터가 station과 fare값이 stationfares[selected_index]에서 나누어지고
+    # 이를 텍스트 형태로 입력창에 넣어주는 함수 
+    # key point: 함수 내에서 selected_index의 "전역변수" 선언해주기()
+    global selected_index   # (중요!) 이 함수안에서 설정된 selected_index가 전역변수임을 알려주어야한다.(만약 그렇지 않을 경우 함수 종료와 함께 값 없어짐)
+    #print('.selection 정보 출력', treeStationfares.selection(), type(treeStationfares.selection()))
     for item in treeStationfares.selection():       # .selection: tree객체(ttk.Treeview 객체)에서 선택된 것들만 가져오는 내장함수
-        selected_index = int(treeStationfares.item(item, "text"))       # treeStationfares의 item중에서 금방가져온 text를 가져오서 int로 변환->selected_index에 할당
+       #print('item 출력',item, type(item))
+       selected_index = int(treeStationfares.item(item, "text"))       # treeStationfares의 item중에서 금방가져온 text를 가져오서 int로 변환->selected_index에 할당
+       #print('.item 출력',treeStationfares.item(item, "text"), type(treeStationfares.item(item, "text")))
     stationfare = stationfares[selected_index]      # stationfares에서 선택된 인덱스의 fare(요금) 값을 가져온다. 
     station = stationfare['station']
     fare = str(stationfare['fare'])
-    text_Station.delete("1.0",END)
-    text_Station.insert("end", station)
-    text_Fare.delete("1.0", END)
-    text_Fare.insert("end", fare)
+    text_Station.delete("1.0",END)      # text_Station: station의 이름을 써주는 곳의 1.0~END까지를 전부 지우는 명령. 즉, 기존에 있던 내용들을 다 지우는 과정
+    text_Station.insert("end", station) # "1.0": 첫번째 줄 첫번째(0번째)문자 // END: 텍스트의 마지막 문자 위치 (Tkinter.Text 문서의 '텍스트 마크' 참고, https://076923.github.io/posts/Python-tkinter-18/)
+    text_Fare.delete("1.0", END)        # .delete(start_index, end_index): start_index부터 end_index까지의 문자열 삭제
+    text_Fare.insert("end", fare)       # .index(index, "문자열"): 해당 index 위치에 문자열 삽입
 
 def setTreeItems(): 
-    # 처음에 실행될 때 or 중견에 변화가 있을 때, stationfares의 값들을 tree에 넣어주는 함수
-    ## 기존에 treeStationfares에 정보를 가져온 것이 있으면 모두 지워줌 & 다시 가져오기(Tip)
-    treeStationfares.delete(*treeStationfares.get_children())   # *treeStationfares.get_children(): treeStationfares에 해당하는 주소값을 모두 가져옴.
+    # 현재 데이터의 정보를 tree에 보여주기위한 setting 함수 
+    # 처음에 실행될 때 or 중간에 변화가 있을 때, stationfares의 값들을 tree에 넣어주는 함수. 즉, tree 내용을 setting 해주는 함수 
+    # setTreeItems는 insert나 update, delete명령 실행 시, 그리고 window.mainloop() 바로 앞에 한 번 씩 실행해 주어야 함.(tree에 보이는 정보를 이벤트에 따라 지속적을 업데이트 해주는 것.)
+    # 기존에 treeStationfares에 정보를 가져온 것이 있으면 모두 지워줌 & 다시 가져오기(Tip)
+    treeStationfares.delete(*treeStationfares.get_children())   # *treeStationfares.get_children(): treeStationfares에 해당하는 주소값을 모두 가져와 지움.
                                                                 # 이것들을 한 번에 확 지워주는 것이 여기 1줄 명령어(데이터가 계속 쌓이는 것을 방지)
-    for idx, stationfare in enumerate(stationfares):
-        station = stationfare['station']
+    for idx, stationfare in enumerate(stationfares):    # 저장된 stationfares 가져오기 
+        station = stationfare['station']                # statoinfares를 station과 fare로 나누기
         fare = stationfare['fare']
-        treeStationfares.insert("", 'end', iid=None, text=str(idx), values=[station, fare]) # text 다시 가져오기
+        treeStationfares.insert("", 'end', iid=None, text=str(idx), values=[station, fare]) # 위에서 가져온 station과 fare를 treeStationfares에 넣어주기
+        # treeStationfares.insert(상위항목, 삽입위치, option)
 
 def insert_content():
-    station = text_Station.get("1.0",END)
+    # 새로운 데이터 추가를 위한 함수 
+    station = text_Station.get("1.0",END)               # station과 fare 입력창의 입력값을 1.0~END까지 가져오기 
     fare = int(text_Fare.get("1.0",END))
-    stationfare = { 'station': station, 'fare': fare }
-    stationfares.append(stationfare)
-    setTreeItems()
+    stationfare = { 'station': station, 'fare': fare }  # stationfare에 station과 fare값을 받아 [dic]형태로 저장
+    stationfares.append(stationfare)                    # stationfares에 데이터 삽입하기 
+    setTreeItems()      # tree의 정보 업데이트
 
 def update_content():
+    # 기존의 데이터를 수정하기 위한 함수
     global selected_index   # (중요!) 이 함수안에서 설정된 selecetd_index가 전역변수임을 알려주어야한다.(만약 그렇지 않을 경우 함수 종료와 함께 값 없어짐)
-    station = text_Station.get("1.0",END)
+    station = text_Station.get("1.0",END)       # 입력창에서 station, fare값 읽어오기 
     fare = int(text_Fare.get("1.0",END))
-    selectedItem = stationfares[selected_index]
-    selectedItem['station'] = station
+    selectedItem = stationfares[selected_index] # tree에서 선택된 데이터의 index값 가져오기 
+    selectedItem['station'] = station           # 해당 데이터의 station, fare값 수정 
     selectedItem['fare'] = fare
-    setTreeItems()
+    setTreeItems()      # tree의 정보 update
 
 def delete_content():
     global selected_index   # (중요!) 이 함수안에서 설정된 selecetd_index가 전역변수임을 알려주어야한다.(만약 그렇지 않을 경우 함수 종료와 함께 값 없어짐)
@@ -65,9 +73,9 @@ def delete_content():
 
 # 전체 화면의 틀 구성하기 
 window = Tk()   # 화면 객체 생성   
-window.title('Station Fare Management') # window의 제목(이름)
-window.geometry("600x600")      # window의 크기(강사님이 설정하심)
-window.resizable(0,0)           # resizable(0,0): 화면크기가 변하는것을 방지?
+window.title('Station Fare Management') # window의 제목(최상단에 표시될 프로그램 제목)
+window.geometry("600x600")      # geometry("너비*높이+x좌표+y좌표"): 윈도우 창의 너비와 높이, 초기 화면 위치의 x좌표,y좌표를 설정할 수 있다.
+window.resizable(0,0)           # resizable(False,False): 화면의 크기를 조정할 수 있게 설정할지에 대한 내용.(True일 때, 조정 가능)
 title = "정류장 요금관리"        # title: 화면에 출력될 제목 내용
 lbl_title = Label(window, text=title, font=("돋움체",20))       #Label(대상객체, text='들어갈 내용', font=("글자체",폰트사아즈)): 화면에 이름을 써주는 tkinter 명령어
 lbl_title.pack(padx=5, pady=15) # pack: 패딩값을 주는 것(x축 y축 여백)
@@ -75,7 +83,7 @@ lbl_title.pack(padx=5, pady=15) # pack: 패딩값을 주는 것(x축 y축 여백
         # 이제 여기에서부터 인형에 눈을 붙이듯이 하나씩 만들어 나갈 것임
 
 
-# 트리구조 설정 및 column(행) 설정
+# 트리구조 설정(목록 표 만들기)
 # treeStationfares 생성 및 설정: 정류장 요금관리를 표시하는 treeStationsfares 생성
 treeStationfares = ttk.Treeview(window)             # ttk.Treeview: 대상객체를 tree 형태로 보여주는 것
 treeStationfares["columns"] = ("station", "fare")   # treeStationfares의 column(열) 설정
@@ -128,6 +136,7 @@ text_Fare.place(x=200, y=500)
 
 # treeStationfares 초기입력(처음에 비어있었던 tree를 초기값 설정으로 채워주기)
 ## setTreeItems: 초기값을 설정해주는 함수, 사용자 정의함수로 직접 설정해 줄 것임.(상단 함수 정의부분 참고) 
+print('stationfares: ', stationfares)
 setTreeItems()
 
 
--- Fastcampus_deep_vible/PART1/GUI_practice.py
+++ Fastcampus_deep_vible/PART1/GUI_practice.py
@@ -1,2 +1,124 @@
-# 즐거운 식자재마트 할인물품 관리
+# Overwatch 영웅들을 관리하는 GUI 프로그램 작성 
 from tkinter import Tk, ttk, Button, Label, Text, END
+
+# 영웅과 class 데이터 입력(charaterclasses)
+characterclasses = [
+    {'character': '겐지', 'class': '공격(Damage)'},
+    {'character': '리퍼', 'class': '공격(Damage)'},
+    {'character': '맥크리', 'class': '공격(Damage)'},
+    {'character': '메이', 'class': '공격(Damage)'},
+    {'character': '바스티온', 'class': '공격(Damage)'},
+    {'character': '솔저:76', 'class': '공격(Damage)'},
+    {'character': '시메트라', 'class': '공격(Damage)'},
+    {'character': '위도우메이커', 'class': '공격(Damage)'},
+    {'character': '정크랫', 'class': '공격(Damage)'},
+    {'character': '토르비욘', 'class': '공격(Damage)'},
+    {'character': '트레이서', 'class': '공격(Damage)'},
+    {'character': '파라', 'class': '공격(Damage)'},
+    {'character': '한조', 'class': '공격(Damage)'},
+    {'character': 'D.va', 'class': '돌격(Tank)'},
+    {'character': '라인하르트', 'class': '돌격(Tank)'},
+    {'character': '레킹볼', 'class': '돌격(Tank)'},
+    {'character': '로드호그', 'class': '돌격(Tank)'},
+    {'character': '윈스턴', 'class': '돌격(Tank)'},
+    {'character': '자리야', 'class': '돌격(Tank)'},
+    {'character': '루시우', 'class': '지원(Surport)'},
+    {'character': '메르시', 'class': '지원(Surport)'},
+    {'character': '젠야타', 'class': '지원(Surport)'}
+]
+
+# tree에서 선택 전역변수
+selected_index = 0
+
+# 핸들 함수 정의
+## 항목 선택 시, 입력창에 데이터 가져오기 
+def characterclasses_selected(event):
+    global selected_index
+    for item in treeCharacterclasses.selection():
+        selected_index = int(treeCharacterclasses.item(item,'text'))
+    characterclass = characterclasses[selected_index]
+    character = characterclass['character']
+    class_ = characterclass['class']
+    text_character.delete('1.0',END)
+    text_character.insert('end',character)
+    text_class.delete('1.0',END)
+    text_class.insert('end',class_)
+
+## Treeview의 데이터 setting하기(추가, 수정, 삭제에 대한 내용 적용하는 setting 함수)
+def setTreecharacters():
+    treeCharacterclasses.delete(*treeCharacterclasses.get_children())
+    for idx, characterclass in enumerate(characterclasses):
+        character = characterclass['character']
+        class_ = characterclass['class']
+        treeCharacterclasses.insert("", 'end', iid=None, text=str(idx), value=[character, class_])
+
+# 데이터 삽입(추가, Insert)를 수행하는 함수
+def insert_content():
+    character = text_character.get('1.0',END)
+    class_ = text_class.get('1.0', END)
+    characterclass = {'character':character, 'class':class_}
+    characterclasses.append(characterclass)
+    setTreecharacters()
+
+# 데이터 수정(Update)를 수행하는 함수 
+def update_content():
+    global selected_index
+    character = text_character.get('1.0', END)
+    class_ = text_class.get('1.0', END)
+    selectedCharacter = characterclasses[selected_index]
+    selectedCharacter['character'] = character
+    selectedCharacter['class'] = class_
+    setTreecharacters()
+
+# 데이터를 삭제(Delete)를 수행하는 함수
+def delete_content():
+    global selected_index
+    characterclasses.pop(selected_index)
+    setTreecharacters()
+
+# GUI 창 설정 
+## window 생성 및 제목 label 생성
+window = Tk()
+window.title('Overwatch Character Manager')
+window.geometry('700x700+50+50')
+window.resizable(0,0)
+title = "Overwatch 캐릭터 종류, class 관리 프로그램"
+lbl_title = Label(window, text=title, font=('koverwatch', 30))
+lbl_title.pack(padx=5 ,pady=25)
+
+## tree 생성
+treeCharacterclasses = ttk.Treeview(window)
+treeCharacterclasses['columns'] = ('character', 'class')
+treeCharacterclasses.column('#0', width=50)
+treeCharacterclasses.heading('#0', text='순번')
+treeCharacterclasses.column('character', width=200)
+treeCharacterclasses.heading('character', text='영웅')
+treeCharacterclasses.column('class', width=200)
+treeCharacterclasses.heading('class', text='분류')
+treeCharacterclasses.place(x=100, y=100, width=500, height=400)
+
+treeCharacterclasses.bind('<<TreeviewSelect>>', characterclasses_selected)
+
+## 버튼 생성: 추가(Insert), 수정(Update), 삭제(Delete) 
+btn_Insert = Button(window, text="추가", command=insert_content, takefocus=True ,font=('koverwatch',16))
+btn_Insert.place(x=100, y=520, width=150, height=30)
+btn_Update = Button(window, text='수정', command=update_content, takefocus=True, font=('koverwatch',16))
+btn_Update.place(x=275, y=520, width=150, height=30)
+btn_Delete = Button(window, text='삭제', command=delete_content,takefocus=True, font=('koverwatch',16))
+btn_Delete.place(x=450, y=520, width=150, height=30)
+
+## 입력창
+labelCharacter = Label(window, text="영웅 이름", justify='left',font=('koverwatch',15))
+labelCharacter.place(x=100, y=580, width=100, height=20)
+labelClass = Label(window, text='분류', justify='left', font=('koverwatch', 15))
+labelClass.place(x=100, y=620, width=100, height=20)
+text_character = Text(window, )
+text_character.place(x=220, y=580, width=320, height=20)
+text_class = Text(window, )
+text_class.place(x=220, y=620, width=320, height=20)
+
+
+## tree 정보 update 
+setTreecharacters()
+
+window.mainloop()
